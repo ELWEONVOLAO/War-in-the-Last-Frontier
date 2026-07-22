6dbe24a9102fc14cf107cc4cbb7b37359e242882
Gabriel Bazan <grbazanr@gmail.com>
2026-07-21 22:31:19 -0400

    a

--- Assets/_Scripts/ScriptCanvas/RoomButton.cs
+++ Assets/_Scripts/ScriptCanvas/RoomButton.cs
@@ -6,25 +6,21 @@ using TMPro;
 
 public class RoomButton : MonoBehaviour
 {
-    [SerializeField] private TextMeshProUGUI roomNameText;
-    [SerializeField] private TextMeshProUGUI pingText; // <-- NUEVO
+    [SerializeField] private TextMeshProUGUI roomNameText; 
+    [SerializeField] private TextMeshProUGUI pingText; 
     [SerializeField] private Button button;
 
     public void Setup(RoomInfo info)
     {
-        // 1. Nombre y Jugadores
         roomNameText.text = info.Name + " (" + info.PlayerCount + "/" + info.MaxPlayers + ")";
         
-        // 2. Ping (Usamos tu ping actual al servidor)
         int miPing = PhotonNetwork.GetPing();
         pingText.text = miPing + " ms";
 
-        // 3. Color según calidad del ping
         if (miPing < 80) pingText.color = Color.green;
         else if (miPing < 150) pingText.color = Color.yellow;
         else pingText.color = Color.red;
 
-        // 4. Botón
         button.onClick.RemoveAllListeners();
         button.onClick.AddListener(() => PhotonNetwork.JoinRoom(info.Name));
     }
